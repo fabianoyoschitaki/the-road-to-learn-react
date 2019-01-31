ccd3d150de82c82760e49ceeb89a7026c0ec3a92
Fabiano <fabianoyoschitaki@gmail.com>
2019-01-31 18:05:44 -0200

    start

--- src/App.js
+++ src/App.js
@@ -2,25 +2,52 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  }
+];
+
 class App extends Component {
   render() {
+    const helloWorld = {
+      text: 'Welcome to React!'
+    };
+
+    const numbers = [1,2,3,4,5];
+    const doubled = numbers.map((number) => number*2);
+    console.log(doubled);
+
+    helloWorld.text = 'Hot Reloading Module';
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div className="App">
+          <h2>{helloWorld.text}</h2>
+          {
+            list.map(item => 
+                <div key={item.objectID}>
+                  <span>
+                    <a href={item.url}>{item.title}</a>
+                  </span>
+                  <span>{item.author}</span>
+                  <span>{item.num_comments}</span>
+                  <span>{item.points}</span>
+                </div>
+            )
+          }
+        </div>
     );
   }
 }
