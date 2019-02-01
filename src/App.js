5f3c142b4cd5bd916c98e8bed42158edd070bb7f
Fabiano <fabianoyoschitaki@gmail.com>
2019-02-01 16:28:56 -0200

    removing

--- src/App.js
+++ src/App.js
@@ -30,36 +30,48 @@ class App extends Component {
       //list: list, because the list variable name is called list, we can declare only list,
       list,
     };
+
+    //The function is bound to the class and thus becomes a class method. You have to bind class methods in the constructor.
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onDismiss(id){
+    /** function isNotId(item){
+      return item.objectID !== id;
+    }
+    const updatedList = this.state.list.filter(isNotId);
+    **/
+    const updatedList = this.state.list.filter(item => item.objectID !== id);
+    this.setState({list: updatedList});
+    console.log(this.state.list);
   }
 
   render() {
     const helloWorld = {
       text: 'Welcome to React!'
     };
-
-    const numbers = [1,2,3,4,5];
-    const doubled = numbers.map((number) => number*2);
-    console.log(doubled);
-
+  
     helloWorld.text = 'Hot Reloading Module';
     return (
         <div className="App">
-          <h2>{helloWorld.text}</h2>
+          <form>
+            <input type="text" />
+          </form>
           {
-            list.map(item => 
-                <div key={item.objectID}>
-                  <span>
-                    <a href={item.url}>{item.title}</a>
-                  </span>
-                  <span>{item.author}</span>
-                  <span>{item.num_comments}</span>
-                  <span>{item.points}</span>
-                  <span>
-                    <button type="button" onClick={() =>  this.onDismiss(item.objectID)}>
-                      Dismiss
-                    </button>
-                  </span>
-                </div>
+            this.state.list.map(item => 
+              <div key={item.objectID}>
+                <span>
+                  <a href={item.url}>{item.title}</a>
+                </span>
+                <span>{item.author}</span>
+                <span>{item.num_comments}</span>
+                <span>{item.points}</span>
+                <span>
+                  <button type="button" onClick={() =>  this.onDismiss(item.objectID)}>
+                    Dismiss
+                  </button>
+                </span>
+              </div>
             )
           }
         </div>
