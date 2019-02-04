9f130d25236c9d87cd007f78215f41c6092914b3
Fabiano <fabianoyoschitaki@gmail.com>
2019-02-04 16:25:25 -0200

    fetch api

--- src/App.js
+++ src/App.js
@@ -80,8 +80,12 @@ class App extends Component {
     }
     const updatedList = this.state.list.filter(isNotId);
     **/
-    const updatedList = this.state.list.filter(item => item.objectID !== id);
-    this.setState({ list: updatedList });
+    const isNotId = item => item.objectID !== id;
+    const updatedHits = this.state.result.hits.filter(isNotId);
+    this.setState({ 
+      //ES5 result: Object.assign({}, this.state.result, { hits: updatedHits })
+      result: { ...this.state.result, hits: updatedHits }
+    });
   }
 
   onSearchChange(event){
